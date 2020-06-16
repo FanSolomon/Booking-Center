c9cb4a56571985b2bdea7b5986ce525d5bd753e7
FanSolomon <jetonlyy@gmail.com>
2020-06-16 18:07:08 +0800

    init project

---
+++ booking-common/src/main/java/com/fansolomon/bookingCommon/BookingCommonApplication.java
@@ -0,0 +1,13 @@
+package com.fansolomon.bookingCommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingCommonApplication.class, args);
+    }
+
+}
---
+++ booking-common/src/test/java/com/fansolomon/bookingCommon/BookingCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fansolomon.bookingCommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
