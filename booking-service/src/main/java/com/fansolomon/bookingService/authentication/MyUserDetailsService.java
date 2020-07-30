bd2e159ce360a28122f18ac23d8fd4053875da94
FanSolomon <jetonlyy@gmail.com>
2020-07-30 21:22:30 +0800

    + password check

--- booking-service/src/main/java/com/fansolomon/bookingService/authentication/MyUserDetailsService.java
+++ booking-service/src/main/java/com/fansolomon/bookingService/authentication/MyUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.fansolomon.bookingService.authentication;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.fansolomon.bookingService.entity.BcUser;
+import com.fansolomon.bookingService.service.IBcUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -7,7 +10,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,7 +21,7 @@ import org.springframework.stereotype.Component;
 public class MyUserDetailsService implements UserDetailsService {
 	
 	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private IBcUserService bcUserService;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -27,10 +29,14 @@ public class MyUserDetailsService implements UserDetailsService {
 		log.info("------登录用户名：{}", username);
 		//根据用户名查找用户信息
 		//这里用了spring security中的用户对象，也可以用自己项目中实际的用户对象(需要实现UserDetails接口)
-		String encodePassword = passwordEncoder.encode("123456");
-		// TODO 待改为查库并进行比对
-		log.info("------加密后的密码：{}", encodePassword);
-		return new User(username, encodePassword,
+		BcUser bcUser = bcUserService.getOne(new QueryWrapper<BcUser>().lambda()
+			.eq(BcUser::getUsername, username));
+		if (bcUser == null) {
+			log.info("------用户:{}不存在", username);
+			throw new UsernameNotFoundException("用户不存在");
+		}
+		log.info("------数据库密码：{}", bcUser.getPassword());
+		return new User(username, bcUser.getPassword(),
 				true, true, true, true,
 				AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
 	}
