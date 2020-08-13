bb9a64d02ea8f4918e445a08cd13484dde271861
FanSolomon <jetonlyy@gmail.com>
2020-08-13 21:48:35 +0800

    config jwt

--- booking-service/src/main/java/com/fansolomon/bookingService/config/AbstractChannelSecurityConfig.java
+++ booking-service/src/main/java/com/fansolomon/bookingService/config/AbstractChannelSecurityConfig.java
@@ -3,8 +3,7 @@ package com.fansolomon.bookingService.config;
 import com.fansolomon.bookingService.properties.BcServiceConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -12,21 +11,13 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
  * @author Colin
  * @since 2020-07-12
  */
-public class AbstractChannelSecurityConfig extends WebSecurityConfigurerAdapter {
+public class AbstractChannelSecurityConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
     protected AuthenticationSuccessHandler myAuthenticationSuccessHandler;
 
     @Autowired
     protected AuthenticationFailureHandler myAuthenticationFailureHandler;
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        //web.ignoring().antMatchers("/**");
-        web.ignoring().antMatchers(BcServiceConstants.DEFAULT_UNAUTHENTICATION_URL,
-                BcServiceConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
-                BcServiceConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*");
-    }
 
     protected void applyPasswordAuthenticationConfig(HttpSecurity http) throws Exception {
         http.formLogin()
--- booking-service/src/main/java/com/fansolomon/bookingService/config/BcServiceSecurityConfig.java
+++ booking-service/src/main/java/com/fansolomon/bookingService/config/BcServiceSecurityConfig.java
@@ -8,12 +8,15 @@ import com.fansolomon.bookingService.validate.code.ValidateCodeSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
@@ -26,7 +29,7 @@ import javax.sql.DataSource;
  */
 //web应用安全配置的适配器
 @Configuration
-@EnableWebSecurity
+@EnableResourceServer
 public class BcServiceSecurityConfig extends AbstractChannelSecurityConfig {
 
     @Autowired
@@ -53,19 +56,46 @@ public class BcServiceSecurityConfig extends AbstractChannelSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public PersistentTokenRepository persistentTokenRepository() {
+//    @Bean
+//    public PersistentTokenRepository persistentTokenRepository() {
+//
+//        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+//        //配置数据源
+//        tokenRepository.setDataSource(dataSource);
+//        //第一次时 配置此项让系统自动创建表
+////		tokenRepository.setCreateTableOnStartup(true);
+//        return tokenRepository;
+//    }
 
-        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
-        //配置数据源
-        tokenRepository.setDataSource(dataSource);
-        //第一次时 配置此项让系统自动创建表
-//		tokenRepository.setCreateTableOnStartup(true);
-        return tokenRepository;
+    /**
+     * 这个bean要与oauth服务AuthorizationServerConfig类tokenStore方法相对应
+     *
+     * @return
+     */
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    /**
+     * 对Jwt签名时，增加一个密钥
+     * JwtAccessTokenConverter：对Jwt来进行编码以及解码的类
+     */
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        // 设置密钥
+        jwtAccessTokenConverter.setSigningKey("colinsprogram");
+        return jwtAccessTokenConverter;
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
 
         super.applyPasswordAuthenticationConfig(http);
 
@@ -79,7 +109,7 @@ public class BcServiceSecurityConfig extends AbstractChannelSecurityConfig {
             //身份认证：表单登录 任何请求都需要身份认证
             .addFilterBefore(tokenCheckFilter, AbstractPreAuthenticatedProcessingFilter.class)
             .rememberMe()
-            .tokenRepository(persistentTokenRepository())
+//            .tokenRepository(persistentTokenRepository())
             .tokenValiditySeconds(securityProperties.getBcService().getRememberMeSeconds())
             .userDetailsService(userDetailsService)
 //		http.httpBasic()
@@ -94,17 +124,5 @@ public class BcServiceSecurityConfig extends AbstractChannelSecurityConfig {
             //暂时关闭csrf 跨站请求伪造防护功能
             .and().csrf().disable();
 
-    }
-
-    /**
-     * 需要配置这个支持password模式 support password grant type
-     *
-     * @return
-     * @throws Exception
-     */
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
     }
 }
